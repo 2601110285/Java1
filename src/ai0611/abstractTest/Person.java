c4fc6b3e154e84c68d7112a6bbbf511e21003ece
2601110285 <2601110285@gspace.kopo.ac.kr>
2026-06-11 10:17:59 +0900

    자바 8장 추상클래스, 인터페이스

---
+++ src/ai0611/abstractTest/Person.java
@@ -0,0 +1,19 @@
+package ai0611.abstractTest;
+
+//  추상클래스
+public abstract class Person {
+    String name;
+    int weight;
+    int height;
+
+    public void eat(String food){
+        System.out.println(food + "을/를 먹는다.");
+    }
+
+    public void sleep(int hour){
+        System.out.println(hour + "시간 잔다.");
+    }
+
+//    추상매소드
+    public abstract void study();
+}
